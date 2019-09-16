7999515eccc5fdc764f2fd1c62f25be1f68a3d95
JackXu <jackxu1999@hotmail.com>
2019-09-15 23:33:00 -0400

    Quiz1

---
+++ src/main/java/edu/emory/cs/utils/queue/LazyPriorityQueue.java
@@ -0,0 +1,35 @@
+package edu.emory.cs.utils.queue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class LazyPriorityQueue<T extends Comparable<T>> extends AbstractPriorityQueue<T> {
+    private List<T> keys;
+
+    public LazyPriorityQueue(Comparator<T> comparator) {
+        super(comparator);
+        keys = new ArrayList<>();
+    }
+
+    public LazyPriorityQueue() { this(Comparator.naturalOrder()); }
+
+    @Override
+    public void add(T key) {
+        keys.add(key);
+    }
+
+    @Override
+    public T remove() {
+        T max = Collections.max(keys, comparator);
+        keys.remove(max);
+        return max;
+    }
+
+    @Override
+    public int size() {
+        return keys.size();
+    }
+}
